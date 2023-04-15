0d0419b7fae39a1a153ec733a6e41fb62222aad2
ZhaoWei Xu <paldier@hotmail.com>
2023-04-15 16:28:07 +0800

    shared:merged model list from asus gpl source code

--- release/src/router/shared/model.c
+++ release/src/router/shared/model.c
@@ -70,7 +70,7 @@ static const struct model_s model_list[] = {
 	{ "GT-AXY16000",	MODEL_GTAXY16000	},
 	{ "GT-AX6000N",		MODEL_GTAX6000N		},
 	{ "RT-AX89U",		MODEL_RTAX89U		},
-	{ "ETJ",			MODEL_ETJ			},
+	{ "ETJ",		MODEL_ETJ		},
 	{ "RT-AX57Q",		MODEL_RTAX57Q		},
 	{ "BLUECAVE",		MODEL_BLUECAVE		},
 	{ "BLUE_CAVE",		MODEL_BLUECAVE		},
@@ -92,7 +92,6 @@ static const struct model_s model_list[] = {
 	{ "ET8PRO",		MODEL_ET8PRO		},
 	{ "RT-AX56_XD4",	MODEL_RTAX56_XD4	},
 	{ "XD4PRO",	MODEL_XD4PRO	},
-	{ "XC5",     		MODEL_XC5		},
 	{ "CT-AX56_XD4",	MODEL_CTAX56_XD4	},
 	{ "RT-AX58U",		MODEL_RTAX58U		},
 	{ "TUF-AX3000",		MODEL_RTAX58U		},
@@ -105,6 +104,7 @@ static const struct model_s model_list[] = {
 	{ "RT-AX82U_V2",	MODEL_RTAX82U_V2	},
 	{ "RT-AX82_XD6S",	MODEL_RTAX82_XD6S	},
 	{ "RT-AX3000N",		MODEL_RTAX3000N		},
+	{ "BR63",		MODEL_BR63		},
 	{ "RT-AX58U_V2",	MODEL_RTAX58U_V2	},
 	{ "TUF-AX3000_V2",	MODEL_TUFAX3000_V2	},
 	{ "RT-AXE7800",		MODEL_RTAXE7800		},
@@ -132,6 +132,11 @@ static const struct model_s model_list[] = {
 	{ "ET8_V2",		MODEL_ET8_V2		},
 	{ "XD6_V2",		MODEL_XD6_V2		},
 	{ "RT-AX5400",		MODEL_RTAX5400		},
+	{ "BC109",		MODEL_BC109		},
+	{ "EBG19",		MODEL_EBG19		},
+	{ "EBG15",		MODEL_EBG15		},
+	{ "EBP15",		MODEL_EBP15		},
+	{ "BC105",		MODEL_BC105		},
 	{ "K3", 			MODEL_K3 			},
 	{ "XWR3100", 		MODEL_XWR3100 		},
 	{ "R7000P", 		MODEL_R7000P 		},
@@ -376,6 +381,7 @@ int is_shared_modelid(int model, char *build_name)
 		if(!strcmp(build_name, "GT-AC2900"))
 			return model + CFID_BASE_2 + 2 + 30;
 		break;
+/*
         case MODEL_RTAX58U:
 		if(!strcmp(build_name, "RT-AX58U"))
 			return model + CFID_BASE_2 + 1 + 30*2;
@@ -392,6 +398,7 @@ int is_shared_modelid(int model, char *build_name)
 		if(!strcmp(build_name, "GS-AX5400"))
 			return model + CFID_BASE_2 + 7 + 30*2;
 		break;
+*/
         case MODEL_RTAX55:
 		if(!strcmp(build_name, "RT-AX55"))
 			return model + CFID_BASE_2 + 1 + 30*3;
@@ -421,18 +428,26 @@ int get_cf_id(int model, char *name) {
 
 	snprintf(tmp, sizeof(tmp), "CF_%s", asus_models_str[model] + strlen("MODEL_"));
 
-	// some models use same modelid
+	// some models use same modelid, specify them in cfid table (comfw.h)
 	if(model == MODEL_RTAX58U) {
 #ifdef RTAX82_XD6
 		return CF_RTAX82_XD6;
+#elif defined(RTAX82U)
+		return CF_RTAX82U;
 #elif defined(TUFAX3000)
 		return CF_TUFAX3000;
+#elif defined(TUFAX5400)
+		return CF_TUFAX5400;
+#elif defined(GSAX3000)
+		return CF_GSAX3000;
+#elif defined(GSAX5400)
+		return CF_GSAX5400;
 #else
 		return CF_RTAX58U;
 #endif
 	}
 
-	for(i = 0; i < MAX_FTYPE; ++i) {
+	for(i = 0; i < MAX_FTYPE; ++i) {	// for those independent modelid and defined in cfid table
 		if((strncmp(tmp, comfw_modid_s[i], strlen(tmp)) == 0) && (strlen(tmp)==strlen(comfw_modid_s[i])))
 			return i;
 	}
@@ -472,4 +487,3 @@ char *get_cf_name(int cfid) {
 */
 
 #endif
-
